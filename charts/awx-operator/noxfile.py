35c30cf176a57a0b6cd77ea374407fe9592934e3
Tzahi Cohen <mjhfvi@gmail.com>
2024-09-01 14:22:27 +0300

    added AWX,  jenkins helmfile changes

---
+++ charts/awx-operator/noxfile.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import nox
+
+
+@nox.session
+def build(session: nox.Session):
+    """
+    Build the AWX Operator docsite.
+    """
+    session.install(
+        '-r',
+        'docs/requirements.in',
+        '-c',
+        'docs/requirements.txt',
+    )
+    session.run(
+        'mkdocs',
+        'build',
+        '--strict',
+        *session.posargs,
+    )
